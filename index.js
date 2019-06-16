2f26966c30d0ac869b5b52800d81f17270c5348c
Zeeshan Zahoor <zahoorzeeshan1@gmail.com>
2019-06-17 00:06:16 +0300

    error message out implemented

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-#!/usr/bin/env node
-
-require('./lib/tsc-watch');
+const TscWatchClient = require('./lib/client');
+const watch = new TscWatchClient();
+watch.on('first_success', () => {
+    console.log('First success!');
+  });
+   
+  watch.on('subsequent_success', () => {
+    console.log('subsequent_success!');
+    });
+   
+  watch.on('compile_errors', (lines) => {
+    console.log('----------------errors-started-------------');
+    console.log(lines);
+    console.log('----------------errors-finished-------------');
+ });
+   
+  watch.start('--project', './example');
